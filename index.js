cc674db3c1e3719c8dfcf0ba98e6321855e387bd
Chris Zubak-Skees <chriszs@gmail.com>
2017-05-27 23:40:26 -0400

    Replace some awful code with some fractionally better code

--- index.js
+++ index.js
@@ -1,195 +1,202 @@
 // jshint esnext:true
 // Started as a PDF.js example file, originally licensed public domain
 
-var fs = require('fs'),
-    util = require('util'),
+const _ = require('highland'),
     dsv = require('d3-dsv'),
+    fs = require('fs'),
     path = require('path'),
-    glob = require('glob');
+    vfs = require('vinyl-fs');
 
 global.DOMParser = require('./lib/domparsermock.js').DOMParserMock;
 
-var pdfjsLib = require('pdfjs-dist');
+var pdfjs = require('pdfjs-dist');
 
-const filePath = `${__dirname}/test/data/`;
-let skipHeaders = {};
+const filingsPath = `${__dirname}/test/data/`; // make these configurable
+    dataPath = `${__dirname}/test/data/`;
 
-function processFiling(pdfPath) {
-    const data = new Uint8Array(fs.readFileSync(pdfPath));
+let skipHeaders = {}; // hmmm
 
-    console.log(pdfPath);
+function getBoldFont(content) {
+    let boldFont = '';
 
-    const tables = [];
-
-    let ignoreRest = false;
-
-    return pdfjsLib.getDocument(data).then(doc => {
-        const numPages = doc.numPages;
-
-        let lastPromise; // will be used to chain promises
-
-        const loadPage = pageNum => doc.getPage(pageNum).then(page => {
-            const viewport = page.getViewport(1.0 /* scale */ );
-
-            return page.getTextContent().then(content => {
-                let boldFont = '';
-
-                const styleKeys = Object.keys(content.styles);
-                styleKeys.forEach(styleKey => {
-                    if (content.styles[styleKey].fontFamily == 'sans-serif' &&
-                        content.styles[styleKey].descent < -0.295) {
-                        boldFont = styleKey;
-                    }
-                });
-
-                let ignorePage = false;
-                let rowY = 0;
-
-                content.items.sort((a, b) => {
-                    if (a.transform[4] - b.transform[4] !== 0) {
-                        return a.transform[4] - b.transform[4];
-                    }
-                    else {
-                        return a.transform[5] - b.transform[5];
-                    }
-                }).forEach((item, i) => {
-                    if (item.str.includes('Data Revised') ||
-                        item.str === 'U.S. Office of Government Ethics Certification') {
-                        ignorePage = true;
-                    }
-
-                    if (!ignorePage && !ignoreRest) {
-                        if (item.height == 14) {
-                            if (item.str == 'Filer\'s Information') {
-                                ignorePage = true;
-                            }
-                            else if (item.str == 'Summary of Contents') {
-                                ignoreRest = true;
-                            }
-                            else if (item.str.trim().length > 2) {
-                                const headerParts = item.str.split('. ');
-                                let part = null;
-                                let name = null;
-
-                                if (headerParts.length > 1) {
-                                    part = parseInt(headerParts[0]);
-                                    name = headerParts.slice(1).join('. ');
-                                }
-                                else {
-                                    name = item.str;
-                                }
-
-                                tables.push({
-                                    part,
-                                    name,
-                                    cols: [],
-                                    rows: []
-                                });
-                            }
-                        }
-                        else if (item.height == 10) {
-                            const curTable = tables[tables.length - 1];
-                            if (curTable) {
-
-                                if (item.fontName == boldFont) {
-                                    if (item.str == 'PART' || (item.str == '#' && curTable.cols.length > 1)) {
-                                        curTable.cols = [];
-                                    }
-
-                                    let append = true;
-
-                                    curTable.cols.forEach(col => {
-                                        if (col.x == item.transform[5]) {
-                                            col.name += ` ${item.str}`;
-                                            col.slug = col.name.toLowerCase().replace(/[ ,]+/g, '-');
-                                            append = false;
-                                        }
-                                    });
-
-                                    if (append) {
-                                        curTable.cols.push({
-                                            name: item.str,
-                                            slug: item.str.toLowerCase().replace(/[ ,]+/g, '-'),
-                                            x: item.transform[5],
-                                            y: item.transform[4]
-                                        });
-                                    }
-                                }
-                                else {
-                                    curTable.cols.forEach((col, i2) => {
-                                        if (col.x == item.transform[5]) {
-                                            if (i2 === 0 && item.transform[4] - rowY > 12) {
-                                                curTable.rows.push({});
-                                                rowY = item.transform[4];
-                                            }
-                                            const curRow = curTable.rows[curTable.rows.length - 1];
-
-                                            if (col.slug in curRow) {
-                                                if (col.slug !== '#') {
-                                                    curRow[col.slug] += ' ';
-                                                }
-                                                curRow[col.slug] += item.str;
-                                            }
-                                            else {
-                                                curRow[col.slug] = item.str;
-                                            }
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                    }
-                });
-
-            });
-        });
-
-        lastPromise = loadPage(2);
-        for (let i = 3; i <= numPages - 4; i++) {
-            lastPromise = lastPromise.then(loadPage.bind(null, i));
+    const styleKeys = Object.keys(content.styles);
+    styleKeys.forEach(styleKey => {
+        if (content.styles[styleKey].fontFamily == 'sans-serif' &&
+            content.styles[styleKey].descent < -0.295) { // can we use a better metric than this?
+            boldFont = styleKey;
         }
-        return lastPromise;
-    }).then(() => {
-        const fileName = path.basename(pdfPath, '.pdf').replace('.PDF','');
-        return new Promise((resolve, reject) => {
-            tables.forEach(table => {
-                const csvFile = `${filePath + table.name.toLowerCase().replace(/[ ,']+/g, '-')}.csv`;
+    });
 
-                table.rows.forEach(row => {
-                    row.file = fileName;
-                });
+    return boldFont;
+}
 
-                if (table.cols.length > 0) {
-                    const columns = ['file'].concat(table.cols.map(col => col.slug));
-                    let csvString = dsv.csvFormat(table.rows, columns);
-                    if (skipHeaders[table.name]) {
-                        csvString = csvString.substring(csvString.indexOf('\n') + 1);
-                    }
-                    else {
-                        skipHeaders[table.name] = true;
-                    }
-                    fs.appendFileSync(csvFile, `${csvString}\n`);
-                }
+function saveFiling(file, tables) {
+    const fileName = file.basename
+        .replace('.pdf', '')
+        .replace('.PDF', '');
+
+    return new Promise((resolve, reject) => {
+        tables.forEach(table => {
+            const csvFile = `${dataPath + table.name.toLowerCase().replace(/[ ,']+/g, '-')}.csv`;
+
+            table.rows.forEach(row => {
+                row.file = fileName;
             });
 
-            resolve();
+            if (table.cols.length > 0) {
+                const columns = ['file'].concat(table.cols.map(col => col.slug));
+                let csvString = dsv.csvFormat(table.rows, columns);
+                if (skipHeaders[table.name]) {
+                    csvString = csvString.substring(csvString.indexOf('\n') + 1);
+                } else {
+                    skipHeaders[table.name] = true;
+                }
+                fs.appendFileSync(csvFile, `${csvString}\n`);
+            }
         });
+
+        resolve();
     });
 }
 
-glob(filePath + '**/*.pdf', function (err, files) {
-    if (err) {
-        throw err;
+function processFiling(file) {
+
+    let tables = [];
+    let ignoreRest = false;
+
+    function processPage(page) {
+        const viewport = page.getViewport(1.0); // scale
+
+        return page.getTextContent()
+            .then(content => {
+                return new Promise((resolve, reject) => {
+                    let boldFont = getBoldFont(content);
+                    let ignorePage = false;
+                    let rowY = 0;
+
+                    content.items.sort((a, b) => {
+                        if (a.transform[4] - b.transform[4] !== 0) {
+                            return a.transform[4] - b.transform[4];
+                        } else {
+                            return a.transform[5] - b.transform[5];
+                        }
+                    }).forEach((item, i) => {
+                        if (item.str.includes('Data Revised') ||
+                            item.str === 'U.S. Office of Government Ethics Certification') {
+                            ignorePage = true;
+                        }
+
+                        if (!ignorePage && !ignoreRest) {
+                            if (item.height == 14) {
+                                if (item.str == 'Filer\'s Information') {
+                                    ignorePage = true;
+                                } else if (item.str == 'Summary of Contents') {
+                                    ignoreRest = true;
+                                } else if (item.str.trim().length > 2) {
+                                    const headerParts = item.str.split('. ');
+                                    let part = null;
+                                    let name = null;
+
+                                    if (headerParts.length > 1) {
+                                        part = parseInt(headerParts[0]);
+                                        name = headerParts.slice(1).join('. ');
+                                    } else {
+                                        name = item.str;
+                                    }
+
+                                    tables.push({
+                                        part,
+                                        name,
+                                        cols: [],
+                                        rows: []
+                                    });
+                                }
+                            } else if (item.height == 10) {
+                                const curTable = tables[tables.length - 1];
+                                if (curTable) {
+
+                                    if (item.fontName == boldFont) {
+                                        if (item.str == 'PART' || (item.str == '#' && curTable.cols.length > 1)) {
+                                            curTable.cols = [];
+                                        }
+
+                                        let append = true;
+
+                                        curTable.cols.forEach(col => {
+                                            if (col.x == item.transform[5]) {
+                                                col.name += ` ${item.str}`;
+                                                col.slug = col.name.toLowerCase().replace(/[ ,]+/g, '-');
+                                                append = false;
+                                            }
+                                        });
+
+                                        if (append) {
+                                            curTable.cols.push({
+                                                name: item.str,
+                                                slug: item.str.toLowerCase().replace(/[ ,]+/g, '-'),
+                                                x: item.transform[5],
+                                                y: item.transform[4]
+                                            });
+                                        }
+                                    } else {
+                                        curTable.cols.forEach((col, i2) => {
+                                            if (col.x == item.transform[5]) {
+                                                if (i2 === 0 && item.transform[4] - rowY > 12) {
+                                                    curTable.rows.push({});
+                                                    rowY = item.transform[4];
+                                                }
+                                                const curRow = curTable.rows[curTable.rows.length - 1];
+
+                                                if (col.slug in curRow) {
+                                                    if (col.slug !== '#') {
+                                                        curRow[col.slug] += ' ';
+                                                    }
+                                                    curRow[col.slug] += item.str;
+                                                } else {
+                                                    curRow[col.slug] = item.str;
+                                                }
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    });
+
+                    resolve();
+                });
+
+            });
     }
 
-    let filingPromise = processFiling(files[0]);
-    for (let pos = 1; pos < files.length; pos++) {
-        filingPromise = filingPromise
-            .then(processFiling.bind(null, files[pos]),
-                console.log);
-    }
-    filingPromise.then(() => {
-        console.log('done');
-    });
+    const data = new Uint8Array(file.contents);
 
-});
+    // parse PDF document
+    return _(pdfjs.getDocument(data))
+        // get each page in the document
+        .flatMap(doc => {
+            let pages = [];
+
+            for (let i = 1; i <= doc.numPages; i++) {
+                pages.push(i);
+            }
+
+            return _(pages)
+                .map(page => _(doc.getPage(page)));
+        })
+        .flatten()
+        .map(processPage)
+        .done(() => {
+            saveFiling(file,tables)
+        });
+}
+
+function processFilings(filingsPath) {
+    _(vfs.src(filingsPath + '**/*.pdf'))
+        .map(processFiling)
+        .done(() => {
+            console.log('done');
+        });
+}
+
+processFilings(filingsPath);
