693d8b0d574b7570ee75a4bd964fffb53f9837b3
Chris Zubak-Skees <chriszs@gmail.com>
2017-05-29 17:54:06 -0400

    Get rid of skipHeaders global state in favor of spamming existsSync to decide whether to write headers to CSV

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const _ = require('highland'),
 const filingsPath = `${__dirname}/test/data/`; // make these configurable
     dataPath = `${__dirname}/test/data/`;
 
-let skipHeaders = {}; // hmmm
-
 function processFilings(path) {
     // process all PDFs in the directory
     _(vfs.src(path + '**/*.@(pdf|PDF)'))
@@ -18,8 +16,6 @@ function processFilings(path) {
             return _(filing.process())
         })
         .flatMap((filing) => {
-            filing.skipHeaders = skipHeaders; // hmmmmmmm
-
             return _(filing.save(dataPath))
         })
         .each((filing) => console.log(filing.file.relative,filing.count))
