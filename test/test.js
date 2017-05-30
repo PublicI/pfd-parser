7f869bf152a27470aeb285d3bff9a79e650e870c
Chris Zubak-Skees <chriszs@gmail.com>
2017-05-30 14:35:25 -0400

    Modify “test”

--- test/test.js
+++ test/test.js
@@ -7,18 +7,27 @@ let should = chai.should();
 
 const kushner = __dirname + '/data/Kushner, Jared.pdf';
 
+// seems like this process should be better encapsulated by the filing parser
+function processFiling(filePath,cb) {
+    _(vfs.src(kushner))
+        .apply((file) => {
+            let filing = new Filing(file);
+
+            filing.process()
+                .then(() => {
+                    cb(null,filing);
+                },(err) => {
+                    cb(err);
+                });
+        });
+}
+
 describe('lib/filing.js', () => {
-    it('should correctly parse Jared Kushner\'s assets', (done) => {
-        _(vfs.src(kushner))
-            .apply((file) => {
-                let filing = new Filing(file);
+    it('should find ten tables in Jared Kushner\'s filing', (done) => {
+       processFiling(kushner,(err,filing) => {
+            filing.tables.length.should.equal(10);
 
-                filing.process()
-                    .then(() => {
-                        filing.tables.length.should.equal(10);
-
-                        done();
-                    });
-            });
+            done();
+       });
     });
 });
