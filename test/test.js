107da04820bf1acca759e91ccf09b048bf38ea8a
Chris Zubak-Skees <chriszs@gmail.com>
2017-06-03 23:43:43 -0400

    Rewrite in-progress

--- test/test.js
+++ test/test.js
@@ -5,15 +5,39 @@ const _ = require('highland'),
 
 let should = chai.should();
 
-const kushner = __dirname + '/data/Kushner, Jared.pdf';
+const filingPath = __dirname + '/data/';
+
+const integrityFiling = filingPath + 'Kushner, Jared.pdf',
+    fdmFiling = filingPath + 'Donnelly, Sally.pdf',
+    fdOnlineFiling = filingPath + 'Mashburn, Lori K.pdf';
 
 describe('lib/parser.js', () => {
-    it('should find 10 tables in Jared Kushner\'s filing', (done) => {
-        parser(kushner)
+    it('should find 10 tables in Integrity filing', (done) => {
+        parser(integrityFiling)
             .then((filings) => {
                 filings[0].tables.length.should.equal(10);
 
                 done();
             });
-    }).timeout(15000);
+    }).timeout(4000);
+
+    it('should find 9 tables in example FDM filing', (done) => {
+        parser(fdmFiling)
+            .then((filings) => {
+                filings[0].tables.length.should.equal(9);
+
+                done();
+            });
+    });
+
+    it('should find 10 tables in example FDonline filing', (done) => {
+        parser(fdOnlineFiling)
+            .then((filings) => {
+                filings[0].tables.length.should.equal(10);
+
+                done();
+            });
+    });
 });
+
+
